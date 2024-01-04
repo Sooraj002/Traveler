d867a60bf3c796b5c8fe0a11f8e310b71b8e7d13
sooraj <soorajnambiar8@gmail.com>
2024-01-04 23:13:54 +0530

    Added basic crud operations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("../Traveler/models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 main()
     .then(() => {
@@ -20,6 +21,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
@@ -51,6 +53,27 @@ app.post("/listings", async(req, res) => {
     res.redirect("/listings");
 });
 
+// Edit Route
+app.get("/listings/:id/edit", async(req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById( id );
+    res.render("listings/edit.ejs", { listing })
+})
+
+// Update Route
+app.put("/listings/:id", async(req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${ id }`);
+})
+
+// Delete Route
+app.delete("/listings/:id", async(req, res) =>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 app.listen(8080, () => {
     console.log("Sever is listening to port 8080");
