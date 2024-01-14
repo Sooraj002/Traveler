14a250391febcb3c39a568a887e6727f365a76f8
sooraj <soorajnambiar8@gmail.com>
2024-01-14 10:57:21 +0530

    added review schema

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const listingSchema = new mongoose.Schema({
+
+const listingSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: {
@@ -18,6 +20,12 @@ const listingSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   location: { type: String, required: true },
   country: { type: String, required: true },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
