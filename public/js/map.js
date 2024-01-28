b936d79179f346fae3c7799727f08018a5c3fdf7
sooraj <soorajnambiar8@gmail.com>
2024-01-28 16:47:43 +0530

    added all map features

--- public/js/map.js
+++ public/js/map.js
@@ -5,12 +5,17 @@ console.log(mapToken);
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v12",
-  center: coordinates,
+  center: coordinates, // Should be an array [lng, lat]
   zoom: 12,
 });
 
-// const marker = new mapboxgl.Marker()
-// .setLngLat(coordinates)
-// .addTo(map);
+// const marker = new mapboxgl.Marker({ color: "red" })
+//   .setLngLat(listing)
+//   .setPopup(new mapboxgl.Popup({ offset: 25 }))
+//   .addTo(map);
 
-const marker = new mapboxgl.Marker({color: "red"}).setLngLat(coordinates).addTo(map);
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates) // Should be an array [lng, lat]
+  .setPopup(new mapboxgl.Popup({ offset: 25 })
+  .setHTML(`<p>Exact location shared after booking</p>`))
+  .addTo(map);
